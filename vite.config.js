19ee367580a7964db875c4c650a3bc307b02f955
Nourhan Fahmy <nourhanfahmy010@gmail.com>
2024-08-05 13:16:56 +0200

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Ecommerce-App/",
+  base: "/ECommerceApp/",
+  // base: "/Ecommerce-App/",
   plugins: [react()],
   assetsInclude: ['**/*.png', '**/*.jpg', '**/*.jpeg', '**/*.gif', '**/*.svg' , '**/*.PNG'],
 

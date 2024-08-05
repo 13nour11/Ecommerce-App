b3ffada21adaf77530cd8a2ed510cc16d90707bb
Nourhan Fahmy <nourhanfahmy010@gmail.com>
2024-08-05 13:38:43 +0200

    .

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/ECommerceApp/",
-  // base: "/Ecommerce-App/",
+  base: "/Ecommerce-App/",
   plugins: [react()],
   assetsInclude: ['**/*.png', '**/*.jpg', '**/*.jpeg', '**/*.gif', '**/*.svg' , '**/*.PNG'],
 
